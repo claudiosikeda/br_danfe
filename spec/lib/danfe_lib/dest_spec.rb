c6d6dc1541dc43c6ff023ff263d2fd8a591f925d
Claudio Ikeda <claudiosikeda@gmail.com>
2018-03-10 19:24:33 -0300

    Include dates to NF-e version 4.00
    Data de Emissao, Data da Entrada/Saida and Hora de Entrada/Saida

--- spec/lib/danfe_lib/dest_spec.rb
+++ spec/lib/danfe_lib/dest_spec.rb
@@ -98,6 +98,48 @@ describe BrDanfe::DanfeLib::Dest do
       end
     end
 
+    
+    context "when nf-e's version is 4.00" do
+      let(:xml_as_string) do
+        <<-eos
+        <NFe xmlns="http://www.portalfiscal.inf.br/nfe">
+          <infNFe Id="NFe25111012345678901234550020000134151000134151" versao="4.00">
+            <ide>
+              <dhEmi>2011-10-29T13:02:59+00:00</dhEmi>
+              <dhSaiEnt>2011-10-30T12:32:45-03:00</dhSaiEnt>
+            </ide>
+            <dest>
+              <CNPJ>82743287000880</CNPJ>
+              <xNome>Schneider Electric Brasil Ltda</xNome>
+              <enderDest>
+                <xLgr>Av da Saudade</xLgr>
+                <nro>1125</nro>
+                <xBairro>Frutal</xBairro>
+                <xCpl>Sala 01 e 02</xCpl>
+                <cMun>3552403</cMun>
+                <xMun>SUMARE</xMun>
+                <UF>SP</UF>
+                <CEP>13171320</CEP>
+                <cPais>1058</cPais>
+                <xPais>BRASIL</xPais>
+                <fone>1921046300</fone>
+              </enderDest>
+              <IE>671008375110</IE>
+            </dest>
+          </infNFe>
+        </NFe>
+        eos
+      end
+
+      it "renders xml to the pdf" do
+        expect(File.exist?(output_pdf)).to be_falsey
+
+        pdf.render_file output_pdf
+
+        expect("#{base_dir}dest#render-v4.00.pdf").to have_same_content_of file: output_pdf
+      end
+    end
+
     context "when recipient has CNPJ" do
       let(:xml_as_string) do
         <<-eos
